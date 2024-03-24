ab1507d55b14324d0b68702ffa74cf6c23ab89ed
alpha21programmer <PANDEYSHUBHAMINDIAN@OUTLOOK.COM>
2024-03-24 22:27:48 +0530

    todo

---
+++ app.js
@@ -0,0 +1,12 @@
+const todoValue = document.getElementById("todoText");
+
+const  listItems = document.getElementById("#list-items");
+
+const  addUpdateClick = document.querySelector("#ADD");
+
+
+
+     
+    
+
+    
