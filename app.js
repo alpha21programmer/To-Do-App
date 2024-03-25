b41673d9efd81ac2d87971ee3569dee12496fb7f
alpha21programmer <PANDEYSHUBHAMINDIAN@OUTLOOK.COM>
2024-03-25 16:58:51 +0530

    changes_done

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-const  listContainer = document.getElementById("list-container");
+const addButton = document.getElementById("button");
 
-const  inputBox = document.querySelector("button");
+const listContainer = document.getElementById("list-container");
+
+const inputBox = document.getElementById("todo-text");
+
+
 
 function addTask() {
-    if (inputBox.value === ''){
+    
+    if (inputBox.value === ""){
         alert("You must write something");
 
     } else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+        const listItem = document.createElement("li");
+        listItem.innerHTML = inputBox.value;
+        listItem.textContent = inputBox.value;
+        listContainer.appendChild(listItem);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        listItem.appendChild(span);
+
         
     }
-
-} ;
+    inputBox.value="";
+    saveData();
+} 
      
-button.addEventlistener("click", addTask());
+addButton.addEventListener("click", addTask);
 
-    
+    listContainer.addEventListener("click", function(e){
+        if(e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveData();
+        }
+        else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    },false);
+
+    function saveData() {
+        localStorage.setItem("myTaskData", listContainer.innerHTML);
+    }
+
+    function showData() {
+        listContainer.innerHTML = localStorage.getItem("myTaskData");
+    }
+    showData();
